841d4ba203b77baadc1cf0739896cdb299c1679c
Milos Gajdos <milosgajdos83@gmail.com>
2018-02-24 02:47:39 +0000

    Small README update.Small refactoring and code comments.

--- README.md
+++ README.md
@@ -4,9 +4,35 @@ Deep Dream experiments
 
 This project is basically a cleaned up rewrite of [keras Deep Dream](https://github.com/keras-team/keras/blob/master/examples/deep_dream.py) with minor addition of image jitters
 
-# How to use
+# Usage
 
-Example run
+```
+usage: creep_dreamz.py [-h] -i INPUT -o OUTPUT [-oct OCTAVE]
+                       [-ocs OCTAVESCALE] [-s STEP] [-iter ITERATIONS]
+                       [-mxl MAXLOSS] [-j JITTER]
+
+Creep Dreamz with Keras.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Path to the input data
+  -o OUTPUT, --output OUTPUT
+                        Path to the output data
+  -oct OCTAVE, --octave OCTAVE
+                        Number of scales at which to run gradient ascent
+  -ocs OCTAVESCALE, --octavescale OCTAVESCALE
+                        Size ratio between scales
+  -s STEP, --step STEP  Gradient ascent step size
+  -iter ITERATIONS, --iterations ITERATIONS
+                        Number of gradient ascent steps per scale
+  -mxl MAXLOSS, --maxloss MAXLOSS
+                        Maximum gradient ascent loss
+  -j JITTER, --jitter JITTER
+                        Pixel shift jitter
+```
+
+# Example run
 
 ```
 creep_dreamz.py -i "input.jpg" -o "dream.png" -oct 4 -ocs 1.2 -s 0.01 -iter 5 -mxl 10.0
--- creep_dreamz.py
+++ creep_dreamz.py
@@ -1,17 +1,27 @@
 import argparse
 import scipy
 import numpy as np
-import tensorflow as tf
 
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications import inception_v3
 from keras import backend as K
 
-def preprocess_image(image_path):
+def preprocess_img(image_path):
     """
-    Loads image from image_path, resizes it to appropriate tensor
-    and preprocesses it for use with InceptionV3 model
-    :param image_path: path to image
+    Preprocess image
+
+    Loads an image, resizes it to and preprocesses it
+    for use with InceptionV3 model
+
+    Parameters
+    ----------
+    image_path : str
+        Path to image on the filesystem.
+
+    Returns
+    -------
+    img : ndarray
+        Preprocessed image
     """
     img = load_img(image_path)
     img = img_to_array(img)
@@ -19,27 +29,21 @@ def preprocess_image(image_path):
     img = inception_v3.preprocess_input(img)
     return img
 
-def deprocess_image(x):
-    """
-    Coverts an image tensor into a numpy image
-    :param x: image tensor
-    """
-    if K.image_data_format() == 'channels_first':
-        x = x.reshape((3, x.shape[2], x.shape[3]))
-        x = x.transpose((1, 2, 0))
-    else:
-        x = x.reshape((x.shape[1], x.shape[2], 3))
-    x /= 2.
-    x += 0.5
-    x *= 255.
-    x = np.clip(x, 0, 255).astype('uint8')
-    return x
-
 def resize_img(img, size):
     """
-    Resize image to appropriate size
-    :param img: image tensor
-    :param size: image dimensions tuple
+    Resize image
+
+    Parameters
+    ----------
+    img: ndarray
+        Input image
+    size: sequence
+        New image size dimensions
+
+    Returns
+    -------
+    img : ndarray
+        Resized image
     """
     img = np.copy(img)
     if K.image_data_format() == 'channels_first':
@@ -53,20 +57,60 @@ def resize_img(img, size):
                    1)
     return scipy.ndimage.zoom(img, factors, order=1)
 
+def deprocess_img(img):
+    """
+    Converts the preprocessed image into original
+
+    Parameters
+    ----------
+    img : ndarray
+        Image data
+
+    Returns
+    -------
+    img : ndarray
+        Image data
+    """
+    if K.image_data_format() == 'channels_first':
+        img = img.reshape((3, img.shape[2], img.shape[3]))
+        img = img.transpose((1, 2, 0))
+    else:
+        img = img.reshape((img.shape[1], img.shape[2], 3))
+    img /= 2.
+    img += 0.5
+    img *= 255.
+    img = np.clip(img, 0, 255).astype('uint8')
+    return img
+
 def save_img(img, fname):
     """
     Saves image on filesystem
-    :params img: image as numpy array
-    :params fname: filename
+
+    Parameters
+    ----------
+    img : ndarray
+        Image data
+    fname : str
+        Filesystem path
     """
-    pil_img = deprocess_image(np.copy(img))
+    pil_img = deprocess_img(np.copy(img))
     scipy.misc.imsave(fname, pil_img)
 
 def build_loss(model, config):
     """
     Builds loss function for gradient ascent
-    :param model: DNN model used for creap dreaming
-    :param config: creeep dream configuration
+
+    Parameters
+    ----------
+    model : keras.Model
+        Keras DNN model used for creep dreaming
+    config : dict
+        Creep dream layer config dictionary
+
+    Returns
+    -------
+    loss : keras.tensor.Op
+        Loss function operation
     """
     # Get the symbolic outputs of each "key" layer (we gave them unique names).
     layer_dict = dict([(layer.name, layer) for layer in model.layers])
@@ -88,9 +132,19 @@ def build_loss(model, config):
 
 def build_gradients(model, loss):
     """
-    Builds gradient tensor
-    :param model: DNN model used for creap dreaming
-    :param loss: gradient ascent loss
+    Builds gradients keras operation
+
+    Parameters
+    ----------
+    model : keras.Model
+        Neural network model used for creap dreaming
+    loss : keras.tensor.Op
+        Gradient ascent loss
+
+    Returns
+    -------
+    grads : keras.tensor.Op
+        Gradients tensor operation
     """
     # dream is created by flowing an input through model
     dream = model.input
@@ -102,12 +156,24 @@ def build_gradients(model, loss):
 
 def dream_shapes(img, octave, octavescale):
     """
-    Reads an image and generates a list of shapes based
-    on supplied octave and octave scale.
-    Returned shapes are sorted in ascending order.
-    :params img: input image as tensor
-    :params octave: number of scales at which to run gradient ascent
-    :params octavescale: size ratio between scales
+    Generates a list of image shapes
+
+    Reads an image and generates a list of image shapes based
+    on supplied octave and octave scale parameters.
+
+    Parameters
+    ----------
+    img : ndarray
+        Input image
+    octave : int
+        Number of scales at which to run gradient ascent
+    octavescale : float
+        Size ratio between shapes
+
+    Returns
+    -------
+    shapes : sequence
+        Sequence of shape sorted in ascending order
     """
     if K.image_data_format() == 'channels_first':
         original_shape = img.shape[2:]
@@ -122,12 +188,25 @@ def dream_shapes(img, octave, octavescale):
 
 def gradient_ascent(x, loss_fn, iterations, step, max_loss=None):
     """
-    Runs gradient ascent for a given loss and input
-    :params x: input data (image)
-    :params loss_fn: function that fetches loss and gradients
-    :params iterations: number of gradient ascent iterations
-    :params step: gradient ascent step size; this scales up the gradient
-    :params max_loss: maximum loss
+    Runs gradient ascent
+
+    Parameters
+    ----------
+    x : ndarray
+        Input data tensor
+    loss_fn : keras.function
+        Function that evaluates and fetches loss and gradients
+    iterations : int
+        Number of gradient ascent iterations
+    step : int
+        Gradient ascent step size; this scales up the gradient size
+    max_loss : float
+        Maximum loss threshold
+
+    Returns
+    -------
+    x : ndarray
+        Output data
     """
     for i in range(iterations):
         # evaluate loss and gradient for the supplied input data
@@ -139,30 +218,53 @@ def gradient_ascent(x, loss_fn, iterations, step, max_loss=None):
         x += step * grad_values
     return x
 
-def dream(img, loss_fn, shapes, iterations, step, max_loss, jitter=32):
+def dream(img, model, iters, step, max_loss, jitter, shapes, config):
     """
     Creep Dream:
-        - Run gradient ascent
-        - Upscale image to the next scale
-        - Reinject the detail that was lost at upscaling time
-    :param img: input image as numpy array
-    :params loss_fn: function that fetches loss and gradients
-    :param shapes: successive image shapes
-    :param iterations: number of iterations
-    :param step: gradient ascent step size
-    :param max_loss: gradient descent max loss
-    :param jitter: pixel offset for more interesting style
+
+    Generates Creep Dream image by running gradient ascent
+    given an input image and loss function
+
+    Parameters
+    ----------
+    img : ndarray
+        Input image
+    model : keras.model
+        Keras CNN model
+    iters : int
+        Number of iterations
+    step : int
+        Gradient ascent step size
+    max_loss : float
+        Gradient descent max loss
+    shapes : sequence
+        Successive image shapes
+    config : dict
+        Model config dictionary
+    jitter : int
+        Pixel shift
+
+    Returns
+    -------
+    img : ndarry
+        Deep dream image
     """
+    # build gradient ascent loss
+    loss = build_loss(model, config)
+    # build image gradients
+    grads = build_gradients(model, loss)
+    # function that evaluates loss and gradients
+    loss_fn = K.function([model.input], [loss, grads])
+
     orig_img = np.copy(img)
     shrunk_orig_img = resize_img(img, shapes[0])
     for shape in shapes:
-        print('Processing image shape', shape)
         img = resize_img(img, shape)
         ox, oy = np.random.randint(-jitter, jitter+1, 2)
         img = np.roll(np.roll(img, ox, -1), oy, -2)
         img = gradient_ascent(img,
                               loss_fn=loss_fn,
-                              iterations=iterations,
+                              iterations=iters,
                               step=step,
                               max_loss=max_loss)
         img = np.roll(np.roll(img, -ox, -1), -oy, -2)
@@ -191,6 +293,8 @@ if __name__ == "__main__":
                         help='Number of gradient ascent steps per scale', required=False)
     parser.add_argument('-mxl', '--maxloss', type=float,
                         help='Maximum gradient ascent loss', required=False)
+    parser.add_argument('-j', '--jitter', type=int, default=32,
+                        help='Pixel shift jitter', required=False)
 
     # These are the names of the InceptionV3 50 layers
     # for which we try to maximize activation,
@@ -210,21 +314,12 @@ if __name__ == "__main__":
     K.set_learning_phase(0)
     # Load ption model
     model = inception_v3.InceptionV3(weights='imagenet', include_top=False)
-    print('Model loaded.')
-    # build gradient ascent loss
-    loss = build_loss(model, config)
-    print('Loss built.')
-    # build image gradients
-    grads = build_gradients(model, loss)
-    print('Gradients built.')
-    # function that evaluates loss and gradients
-    loss_fn = K.function([model.input], [loss, grads])
     # preprocess input image
-    img = preprocess_image(args.input)
-    print('Preprocessed image', args.input)
+    img = preprocess_img(args.input)
     # generate creep dream shapes
     shapes = dream_shapes(img, args.octave, args.octavescale)
     # run creep dream and get the resulting image
-    img = dream(img, loss_fn, shapes, args.iterations, args.step, args.maxloss)
+    img = dream(img, model, args.iterations, args.step,
+                args.maxloss, args.jitter, shapes, config)
     # save resulting image to hard drive
     save_img(img, fname=args.output)
